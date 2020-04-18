2211f928ea23a76ef95f05d6eea0a60bcf8d91e3
anishniroula <anishniroula1@gmail.com>
2020-04-18 18:51:29 -0400

    init create-react-library@3.0.7

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'form-validation-react'
+import 'form-validation-react/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent/>
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import styles from './styles.module.css'
+import useForm from './library/utils/useForm'
+import validate, { userQuestions } from './library/userQuestions'
+import TextboxQuestion from './library/questions/textbox-question/textbox-question'
 
-ReactDOM.render(
-  <React.StrictMode>
-          <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+export const ExampleComponent = ({ text }) => {
+  const submitFunction = async () => {
+    console.log(values)
+  }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  const initialState = { email: 'email@gmail.com', phone: '1234567890' }
+  // call the hook
+  const { handleSubmit, handleChange, handleBlur, values, errors } = useForm(
+    validate,
+    submitFunction,
+    initialState
+  )
+  const question = userQuestions(values, errors)
+  return (
+    <div>
+      <TextboxQuestion
+        question={question.email}
+        onChange={handleChange}
+        onBlur={handleBlur}
+      />
+    </div>
+  )
+}
