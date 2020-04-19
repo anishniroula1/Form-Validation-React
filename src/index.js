710e9c18788db8b573cc1a3ce3ad0de9004b3f6e
anishniroula <anishniroula1@gmail.com>
2020-04-19 10:49:09 -0400

    Little Change

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import './styles.module.scss'
 export * from './library/button/Button'
 export * from './library/common/validation-message/validation-message'
 export * from './library/questions/dropdown-question/dropdown-question'
