51499101484a329c20f48d956bbc577f2bbd07e2
anishniroula <anishniroula1@gmail.com>
2020-04-18 22:38:26 -0400

    Little Change

--- src/library/utils/form-error-summary/form-error-summary.js
+++ src/library/utils/form-error-summary/form-error-summary.js
@@ -1,22 +1,23 @@
-import React from "react";
+import React from 'react'
 import './form-error-summary.scss'
-export default function FormErrorSummary({errors}) {
-    return (
-        <div>
-        {Object.keys(errors).length > 0 &&
-                <div className={errors ? "form-error-summary__overall" : ''}>
-                    <p className="form-error-summary__container">
-            <span className="form-error-summary__svg">
-                <span className="form-error-summary__error">
-                    Looks like {Object.keys(errors).length} Error has occured, please Check Below Field
-                </span>
+export function FormErrorSummary({ errors }) {
+  return (
+    <div>
+      {Object.keys(errors).length > 0 && (
+        <div className={errors ? 'form-error-summary__overall' : ''}>
+          <p className='form-error-summary__container'>
+            <span className='form-error-summary__svg'>
+              <span className='form-error-summary__error'>
+                Looks like {Object.keys(errors).length} Error has occured,
+                please Check Below Field
+              </span>
             </span>
-                    </p>
-                </div>
-        }
+          </p>
         </div>
-    );
-    /*return (
+      )}
+    </div>
+  )
+  /* return (
         <div className="warning">
             <div className="warning__image"></div>
             <div className="warning__content">
@@ -25,5 +26,5 @@ export default function FormErrorSummary({errors}) {
                 </p>
             </div>
         </div>
-    )*/
-};
+    ) */
+}
